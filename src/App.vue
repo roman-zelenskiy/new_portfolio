837a4f751b32a258797481f16c37f82db3d79090
Roman <romazelenskiy2000@gmail.com>
2023-08-30 01:57:52 +0300

    first_commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import Navigation from './components/Navigation.vue';
+</script>
+
+<template>
+    <div class="wrapper_app">
+        <navigation />
+        <div class="pages_section">
+            <h1>hello</h1>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    @import './assets/styles/app.scss';
+</style>
