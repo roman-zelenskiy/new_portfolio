371599dfdeded07b2a28b6a9a2546c63148cddc6
Roman <romazelenskiy2000@gmail.com>
2023-08-31 01:00:40 +0300

    add markup navigation component

---
+++ src/components/Navigation/SocialNetwork.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SocialNetwork"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/PagesSection/PagesSection.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PagesSection"
+}
+</script>
+
+<style scoped>
+
+</style>
