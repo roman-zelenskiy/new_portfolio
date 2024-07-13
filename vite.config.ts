ac2124cf6f7d9a0b42e8c33021584a126fda6629
Roman <romazelenskiy2000@gmail.com>
2024-07-13 22:55:21 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd());
+    // const env = loadEnv(mode, process.cwd());
 
     return {
         plugins: [vue()],
-        base: env.VITE_BASE_URL,
+        base: '/new_portfolio',
         server: {
             open: '/',
         },
