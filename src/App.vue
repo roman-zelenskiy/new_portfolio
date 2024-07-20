faece32a8cb6b6d7d57dc64ede16072d895ed10a
Roman <romazelenskiy2000@gmail.com>
2024-07-20 21:02:20 +0300

    fix deploy

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
     import { ref, provide, toRef } from "vue";
     import userData from "../src/data/userData.json";
 
-    console.log(import.meta.env);
-    console.log(import.meta.env.MODE);
-    console.log(import.meta.env.VITE_ADMIN_PASSWORD);
-    console.log(import.meta.env.VITE_BASE_URL);
-    console.log(import.meta.env.VITE_TELEGRAM_BOT_TOKEN);
-    console.log(import.meta.env.GITHUB_TOKEN);
-
     // const userData = ref();
     const socialNetworks = ref<any[]>([]);
 
