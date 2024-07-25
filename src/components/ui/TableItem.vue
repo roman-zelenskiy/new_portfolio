5501180d6ef37a45ee6efd14ef145626690a6207
Roman <romazelenskiy2000@gmail.com>
2024-07-26 01:07:12 +0300

    test github api

--- src/components/ui/TableItem.vue
+++ src/components/ui/TableItem.vue
@@ -97,7 +97,7 @@
             >
                 <button
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    :class="action.styles"
+                    :class="action?.styles"
                     v-for="action in td.actions"
                     @click="action.action"
                 >
