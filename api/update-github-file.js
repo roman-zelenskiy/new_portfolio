3bb81f7b3abc6cc36cbcb8156a2dd8e56d079d81
Roman <romazelenskiy2000@gmail.com>
2024-07-26 00:31:40 +0300

    test github api

---
+++ api/update-github-file.js
@@ -0,0 +1,34 @@
+const { Octokit } = require("@octokit/core");
+
+module.exports = async (req, res) => {
+  const octokit = new Octokit({
+    auth: process.env.PERSONAL_ACCESS_TOKEN,
+  });
+
+  const { newUserData } = req.body;
+
+  try {
+    const repoOwner = "roman-zelenskiy";
+    const repoName = "new_portfolio";
+    const filePath = "src/data/test.json";
+    const fileContent = JSON.stringify(newUserData);
+
+    const { data: { sha } } = await octokit.request(`GET /repos/${repoOwner}/${repoName}/contents/${filePath}`);
+
+    const newContent = Buffer.from(fileContent).toString('base64');
+
+    await octokit.request(`PUT /repos/${repoOwner}/${repoName}/contents/${filePath}`, {
+      owner: repoOwner,
+      repo: repoName,
+      path: filePath,
+      message: "Update file via GitHub Action",
+      content: newContent,
+      sha: sha,
+    });
+
+    res.json({ message: "File updated successfully" });
+  } catch (error) {
+    console.error("Error updating file:", error);
+    res.status(500).json({ error: "Error updating file" });
+  }
+};
