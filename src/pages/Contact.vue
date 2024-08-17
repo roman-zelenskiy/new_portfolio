582317345225fa32cf8970bf4f2edb60d147a3a0
Roman <romazelenskiy2000@gmail.com>
2024-08-18 01:00:05 +0300

    fix

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
+  import { computed, inject, ref } from 'vue';
   import { useToggle } from '@vueuse/core';
-  const [isModalSuccess, toggleModalSuccess] = useToggle();
-  const [isModalDanger, toggleModalDanger] = useToggle();
   import PrimaryButton from '../components/ui/PrimaryButton.vue';
   import ModalSuccess from '../components/common/ModalSuccess.vue';
   import ModalDanger from '../components/common/ModalDanger.vue';
-  import { computed, inject, ref } from 'vue';
+
+  const [isModalSuccess, toggleModalSuccess] = useToggle();
+  const [isModalDanger, toggleModalDanger] = useToggle();
 
   const userData: any = inject('userData');
   const socialNetworks: any = inject('socialNetworks');
@@ -47,41 +48,43 @@
 </script>
 
 <template>
-  <ModalSuccess v-model="isModalSuccess" :message="messageToUser"></ModalSuccess>
-  <ModalDanger v-model="isModalDanger" :message="messageToUser"></ModalDanger>
-  <div class="contact_page">
-    <div class="content_box">
-      <h5 class="page_title">Contact</h5>
-      <p class="text_contact">Reach out me</p>
-      <p class="residence">{{ userData?.city }}, {{ userData?.county }}</p>
-      <ul class="gmail_phone">
-        <li class="phone_number">
-          <a :href="`tel: ${userData?.phone_number}`">{{ userData?.phone_number }} </a>
-        </li>
-        <li class="email">
-          <a :href="`mailto: ${userData?.email}`">
-            {{ userData?.email }}
-          </a>
-        </li>
-      </ul>
-      <ul class="social_networks">
-        <li v-for="item in socialNetworks">
-          <a :href="item?.path">{{ item?.title }}</a>
-        </li>
-      </ul>
-    </div>
-    <div class="form_box">
-      <h5 class="title_form">any project?</h5>
-      <form action="#" @submit.prevent="onSendForm()">
-        <div class="form_wrap">
-          <div class="input_box">
-            <input class="name" required type="text" v-model="name" placeholder="Name" />
-            <input class="email" required type="email" v-model="email" placeholder="Email" />
-            <textarea v-model="message" required class="message" placeholder="Message"></textarea>
+  <div>
+    <ModalSuccess v-model="isModalSuccess" :message="messageToUser"></ModalSuccess>
+    <ModalDanger v-model="isModalDanger" :message="messageToUser"></ModalDanger>
+    <div class="contact_page">
+      <div class="content_box">
+        <h5 class="page_title">Contact</h5>
+        <p class="text_contact">Reach out me</p>
+        <p class="residence">{{ userData?.city }}, {{ userData?.county }}</p>
+        <ul class="gmail_phone">
+          <li class="phone_number">
+            <a :href="`tel: ${userData?.phone_number}`">{{ userData?.phone_number }} </a>
+          </li>
+          <li class="email">
+            <a :href="`mailto: ${userData?.email}`">
+              {{ userData?.email }}
+            </a>
+          </li>
+        </ul>
+        <ul class="social_networks">
+          <li v-for="item in socialNetworks">
+            <a :href="item?.path">{{ item?.title }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="form_box">
+        <h5 class="title_form">any project?</h5>
+        <form action="#" @submit.prevent="onSendForm()">
+          <div class="form_wrap">
+            <div class="input_box">
+              <input class="name" required type="text" v-model="name" placeholder="Name" />
+              <input class="email" required type="email" v-model="email" placeholder="Email" />
+              <textarea v-model="message" required class="message" placeholder="Message"></textarea>
+            </div>
+            <PrimaryButton class="send_btn"> Submit now </PrimaryButton>
           </div>
-          <PrimaryButton class="send_btn"> Submit now </PrimaryButton>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </template>
